6ce84d31df787d13c56f0dca543253c5993cbb06
steventai <twnwcl@gmail.com>
2012-10-12 11:09:37 +0800

    modify data display

--- PhoneAppSkin/ContrastController.m
+++ PhoneAppSkin/ContrastController.m
@@ -108,16 +108,7 @@
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    /*
-     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-     
-     if (cell == nil) {
-     //cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-     //        cell = [[[NSBundle mainBundle] loadNibNamed:@"NewCell" owner:self options:nil] lastObject];
-     UINib *theNib = [UINib nibWithNibName:@"NewCell" bundle:nil];
-     cell = [[theNib instantiateWithOwner:self options:nil] lastObject];
-     }
-     */
+
     // Configure the cell...
     NSString *phonename = [self.phraseData objectAtIndex:indexPath.row %8];
     NSString *phonenum = [self.tableData objectAtIndex:indexPath.row %8];
@@ -130,69 +121,21 @@
     [theTitleLabel setText:phonename];
     [theSubtitle setText:phonenum];
 
-  //  [theTitleLabel setText:[NSString stringWithFormat:@"Row %d", indexPath.row]];
-  //  [theSubtitle setText:[NSString stringWithFormat:@"Number %d", indexPath.row]];
         
     UIImageView *myImageView = (UIImageView *)[cell viewWithTag:1030];
     [myImageView setImage:[UIImage imageNamed:@"contast-bk.png"]];
     
-    //UIImageView *cellBackground = (UIImageView *)[cell viewWithTag:1020];
-    //cellBackground.image = [UIImage imageNamed:@"yelp"];
     return cell;
 
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     [detailViewController release];
-     */
 }
 
 @end
